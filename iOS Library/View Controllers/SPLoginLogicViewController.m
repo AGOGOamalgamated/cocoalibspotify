c847263d0a1148387d353c621a253cedeabd1b44
ChristopherRyanCruz <chris@agogo.com>
2013-11-26 11:59:24 -0800

    fix for form underlapping the login navbar
    Fixed an issue where in certain circumstances, the login form in
    SPLoginLogicViewController would underlap the navigation bar. This
    issue can be reproduced by attempting to use this login controller
    running an iPhone-native app on an iPad. This fix prevents the edges
    from being extended, ensuring that the login form never gets
    underlapped.

--- iOS Library/View Controllers/SPLoginLogicViewController.m
+++ iOS Library/View Controllers/SPLoginLogicViewController.m
@@ -190,6 +190,8 @@
 	
 	// Login form background
 	
+    self.edgesForExtendedLayout = UIRectEdgeNone;
+    
 	UIView *loginContainerView = [[UIView alloc] initWithFrame:CGRectMake(10.0, 20.0, 300.0, 185.0)];
 	loginContainerView.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin;
 	[self.view addSubview:loginContainerView];
